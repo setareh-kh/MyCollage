fd5f256054afb4cb51a80aa624baf4e687f513b3
DESKTOP-NBKS71J\Star <setareh.hoh@gmail.com>
2024-02-12 18:19:08 +0330

    no message

--- DTO/Responses/StudentResponseDto.cs
+++ DTO/Responses/StudentResponseDto.cs
@@ -3,7 +3,7 @@ namespace MyCollage_EF_Rep_AsyncAwait.DTO.Responses
     public class StudentResponseDto
     {
         public int Id {get ; set;}
-        public string FullName { get; set; }
-        public IFormFile Image { get; set; }
+        public string? FullName { get; set; }
+        public IFormFile? Image { get; set; }
     }
 }
